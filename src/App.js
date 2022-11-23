1cc813f9e8ede85bc7be32d6a51710a2aaaf7917
PIJUSH364 <praymondal@gmail.com>
2022-11-23 22:55:58 +0530

    this is all about modal coomponent

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Modal from './Modal';
 
 function App() {
-  return <div>Home 😃</div>;
+  return (
+    <Modal>
+      <div>
+        <h1>headline</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum ex
+          amet distinctio, saepe quo nesciunt ipsa voluptatibus quia deleniti
+          assumenda, excepturi repellat veritatis.
+        </p>
+      </div>
+    </Modal>
+  );
 }
 
 export default App;
