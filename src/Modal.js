d4d3fbef6ed61d8ca226120b2efb8545313ee64b
PIJUSH364 <praymondal@gmail.com>
2022-11-23 22:56:30 +0530

    End up chapter one

---
+++ src/Modal.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import CloseIcon from '@mui/icons-material/Close';
+const ModalBackground = styled.div`
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+const ModalBody = styled.div`
+  background-color: white;
+  margin: 10% auto;
+  padding: 20px;
+  width: 50%;
+`;
+function Modal({ children }) {
+  const [shouldShow, setShouldShow] = useState(false);
+  return (
+    <>
+      <button onClick={() => setShouldShow(true)}>show model</button>
+      {shouldShow && (
+        <ModalBackground onClick={() => setShouldShow(false)}>
+          <ModalBody onClick={(e) => e.stopPropagation()}>
+            <button onClick={() => setShouldShow(false)}>Hide model</button>
+            {children}
+          </ModalBody>
+        </ModalBackground>
+      )}
+    </>
+  );
+}
+
+export default Modal;
