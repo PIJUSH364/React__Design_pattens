efa4076c1ff2be3e8ff18add3f4438fc8e93f59c
PIJUSH364 <praymondal@gmail.com>
2022-11-24 15:59:16 +0530

    noe we controlled over modal component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ControlledForm from './ControlledForm';
+import ControlledModal from './ControlledModal';
 import Modal from './Modal';
 import UncontrolledForm from './UncontrolledForm';
 
 function App() {
+  const [shouldShowModel, setShouldShowModel] = useState(false);
   return (
     <>
       {/* <UncontrolledForm /> */}
       {/* <ControlledForm /> */}
-      <Modal>
+      {/* <Modal>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe atque
           dolore dicta voluptatum!
         </p>
-      </Modal>
+      </Modal> */}
+      <ControlledModal
+        shouldShow={shouldShowModel}
+        onRequestClose={() => setShouldShowModel(false)}
+      >
+        <h3>controlled component</h3>
+      </ControlledModal>
+      <button onClick={() => setShouldShowModel(!shouldShowModel)}>
+        {shouldShowModel ? 'Hide Modal' : 'Show Modal'}
+      </button>
     </>
   );
 }
